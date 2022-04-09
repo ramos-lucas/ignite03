bb417a0becf230967437be12ef0655ea882813ab
Lucas Ramos <ramos.lucash@gmail.com>
2022-04-09 20:39:49 -0300

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): React.ReactElement {
+  return (
+    <div className={styles.container}>
+      <div className={commonStyles.pageContainer}>
+        <Link href="/">
+          <a>
+            <img src="/spacetraveling.svg" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 }
